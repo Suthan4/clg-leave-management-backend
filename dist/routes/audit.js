5e5e870f77c42e2fc3dfacce7a21af721862f547
Suthan4 <harisuthan268@gmail.com>
2026-03-08 16:45:08 +0530

    Remove ignored files and apply .gitignore

--- dist/routes/audit.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auditController_1 = require("../controllers/auditController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.get('/', auth_1.protect, auth_1.adminOnly, auditController_1.getAuditLogs);
-exports.default = router;
--- dist/routes/settings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const settingsController_1 = require("../controllers/settingsController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.get('/', auth_1.protect, settingsController_1.getSettings);
-router.put('/', auth_1.protect, auth_1.adminOnly, settingsController_1.updateSettings);
-exports.default = router;
